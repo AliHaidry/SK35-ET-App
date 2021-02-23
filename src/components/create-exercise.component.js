65928bc50de12f5f11ab13ff798432389795b803
AliHaidry <alihaidry11@gmail.com>
2021-02-22 21:16:49 -0500

    code update

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -24,7 +24,7 @@ export default class CreateExercise extends Component {
 
   
   componentDidMount() {
-    axios.get("https://app-exercisetracker-backend.herokuapp.com/users/")
+    axios.get("https://app-exercisetracker-backend.herokuapp.com/users")
       .then(response => {
         if (response.data.length > 0) {
           this.setState({
